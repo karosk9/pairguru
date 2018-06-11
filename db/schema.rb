c1933e50b6c73f308d56a3a81326a5b34d20993e
Karolina <karosk9@gmail.com>
2018-06-12 00:09:02 +0200

    define associations between users comments and movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_205329) do
+ActiveRecord::Schema.define(version: 2018_06_11_215205) do
 
   create_table "comments", force: :cascade do |t|
     t.string "title"
@@ -18,7 +18,9 @@ ActiveRecord::Schema.define(version: 2018_06_11_205329) do
     t.integer "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["movie_id"], name: "index_comments_on_movie_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "genres", force: :cascade do |t|
