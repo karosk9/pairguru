5ad74fbd19560d862af5665897c3012f3c4277af
Karolina <karosk9@gmail.com>
2018-06-12 00:09:47 +0200

    create basic actions for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,28 @@
 class CommentsController < ApplicationController
+
+before_action :provide_movie, only: [:create, :destroy]
+
   def create
+    @comment = Comment.new(comment_params.merge(movie: @movie))
+    if @comment.save
+      redirect_to movie_path(@movie)
+    end
   end
 
   def destroy
+    @comment = @movie.comments.find(params[:id])
+    @comment.destroy
+    redirect_to movie_path(@movie)
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:title, :body, :movie_id)
+  end
+
+  def provide_movie
+    @movie = Movie.find(params[:movie_id])
   end
 end
